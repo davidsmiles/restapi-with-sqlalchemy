854d71f4d16244e4a53630869f23770c945a5447
David Smiles <davidugbero@gmail.com>
2019-10-05 20:40:17 +0100

    remove if __name__ == '__main__' condition

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 app.config['JWT_AUTH_USERNAME_KEY'] = 'username'
 
 
+@app.before_first_request
+def create_table():
+    DB.create_all()
+
+
 api.add_resource(Index, '/')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(ItemList, '/items')
@@ -34,8 +39,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(UserRegister, '/signup')
 
 
-if __name__ == '__main__':
-    from DB import DB
-    DB.init_app(app)
+from DB import DB
+DB.init_app(app)
 
-    app.run(port=5000, debug=True)
+app.run(port=5000, debug=True)
